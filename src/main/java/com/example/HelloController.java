2e018e6888322b50b2321862b789266577c76054
Don Subert <helo478@gmail.com>
2018-03-02 15:53:17 -0800

    added API security with pre-post authorization

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,12 +1,14 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
+    @PreAuthorize("isAuthenticated()")
     public String index() {
         return "Greetings from Spring Boot!";
     }
